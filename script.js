9d2d96d6c98c85693bdd63e08bfa86d9745f4bca
Amit swain <amitkuswain6@gmail.com>
2024-10-10 16:16:25 +0530

    Portfolio-project

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('form').addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if(name && email && message){
+        alert("Thank you, "+ name + "! Your message has been sent.");
+
+    }else{
+        ("Please fill in all the fields.")
+    }
+})
